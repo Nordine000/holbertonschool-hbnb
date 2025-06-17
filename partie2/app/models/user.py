55bfe1e41fdea0376a2173ff353c43f51d27dec5
Nordine000 <ounissi970@gmail.com>
2025-06-17 02:40:56 +0200

    creation de user

---
+++ partie2/app/models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+
+
+class User:
+    def __init__(self, first_name, last_name, email, is_admin=False, created_at=None, updated_at=None, id=None):
+        self.__id__ = str(uuid4())
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.__created_at__ = datetime.now()
+        self.__updated_at = datetime.now()
