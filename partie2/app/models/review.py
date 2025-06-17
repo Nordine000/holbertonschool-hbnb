2515dd8a8f0c832f83f7979b95c9db52a2b013f4
Nordine000 <ounissi970@gmail.com>
2025-06-18 01:06:32 +0200

    creation de rewiew

---
+++ partie2/app/models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+from user import User
+from place import Place
+
+class Review:
+    def __init__(self, text, place, rating=int, user=User,created_at=None, updated_at=None, id=None):
+        self.__id__ = str(uuid4())
+        self.text = text
+        self.place = place
+        self.rating = rating
+        self.user = user
+        self.__created_at__ = datetime.now()
+        self.__updated_at = datetime.now()
