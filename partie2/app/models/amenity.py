0714c17cdd169a5aff75f5450d5879a45f013d41
Nordine000 <ounissi970@gmail.com>
2025-06-18 01:11:27 +0200

    creation de amenity

---
+++ partie2/app/models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+
+class Amenity:
+    def __init__(self, name, created_at=None, updated_at=None, id=None):
+        self.id = str(uuid4())
+        self.name = name
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
