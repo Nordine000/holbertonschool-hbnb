7bc2a26f18fe2cc65f831facf7f67e653fc4a5d1
Nordine000 <ounissi970@gmail.com>
2025-06-22 18:59:32 +0200

    test de la tache 3 reussie

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,8 +1,7 @@
 from flask_restx import Api
 from app.api.v1.amenities import api as amenities_ns
 # Import other namespaces as needed
-from api.v1.users import api as users_ns
-from api.v1.places import api as places_ns
+from app.api.v1.users import api as users_ns
 
 api = Api(
     title='HBnB Evolution API',
@@ -14,4 +13,4 @@ api = Api(
 # Register namespaces
 api.add_namespace(amenities_ns, path='/api/v1/amenities')
 api.add_namespace(users_ns, path='/api/v1/users')
-api.add_namespace(places_ns, path='/api/v1/places')
+#capi.add_namespace(places_ns, path='/api/v1/places')
